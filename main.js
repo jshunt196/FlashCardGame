952cd6d8024548763d9150a38e0e769835380aff
justintunev7 <37857239+justintunev7@users.noreply.github.com>
2018-09-19 11:41:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var ctr = 1;
 var health;
+var currentQuestion = 0;
 
 function loseHealth() {
   health = 100 - (25 * ctr++);
@@ -28,9 +29,24 @@ function inputQuestions() {
 
   questions.push(prompt("What is your fifth question?"));
   answers.push(prompt("What is the answer to the question?"));
+
+  qTransition(0);
 }
 
 function qTransition(questionNum) {
     document.getElementById("question").innerHTML = questions[questionNum];
     document.getElementById("answer").innerHTML = answers[questionNum];
+    currentQuestion = questionNum; // change question counter
+}
+
+function compareValues() {
+    var x = document.getElementById("frm1");
+    alert(x.elements[0].value);
+
+    if (x.elements[0].value == answers[currentQuestion]) {
+      document.getElementById("answer").innerHTML = answers[currentQuestion] + "\nGood Job!";
+    }
+    else {
+      loseHealth();
+    }
 }
