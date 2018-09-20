dba3cd4203fc7984b3e59231813b06f330f8af25
jshunt196 <43077170+jshunt196@users.noreply.github.com>
2018-09-19 20:43:41 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 var ctr = 1;
 var health;
 var currentQuestion = 0;
+var isFlipped = false;
+
+var input1 = document.getElementById("input1");
+
+var answer1 = document.getElementById("answer1");
+
+var input2 = document.getElementById("input2");
+
+var answer2 = document.getElementById("answer2");
+
+var input3 = document.getElementById("input3");
+
+var answer3 = document.getElementById("answer3");
+
+var input4 = document.getElementById("input4");
+
+var answer4 = document.getElementById("answer4");
+
+var input5 = document.getElementById("input5");
+
+var answer5 = document.getElementById("answer5");
 
 function loseHealth() {
   health = 100 - (25 * ctr++);
@@ -48,4 +69,37 @@ function compareValues() {
     else {
       loseHealth();
     }
+}
+
+function keyEnter(input) {
+  
+  if (event.key === 'Enter' && !isFlipped) {
+    event.preventDefault();
+    document.getElementById("cardFront").classList.toggle('flipped');
+    compareValues();
+    isFlipped = true;
+  }
+}
+
+function flipBack() {
+  if (isFlipped) {
+    document.getElementById("cardFront").classList.toggle('flipped');
+    isFlipped = false;
+  }
+}
+
+function wut() {
+  questions.push(input1);
+  answers.push(answer1);
+  questions.push(input2);
+  answers.push(answer2);
+  questions.push(input3);
+  answers.push(answer3);
+  questions.push(input4);
+  answers.push(answer4);
+  questions.push(input5);
+  answers.push(answer5);
+  alert(questions.length);
+  currentQuestion = 0;
+  window.location.href='index.html';
 }
